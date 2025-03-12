86f455cc21020797f0e7d1cdde659e4b0f599407
Joseph Boucher <101840777+The-real-joe@users.noreply.github.com>
2025-03-12 01:16:28 +0000

    logo

--- home_page.py
+++ home_page.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.markdown("# Welcome to the East Coast Coven")
 st.sidebar.markdown("# Home")
 
-st.image("/workspaces/East_Coast_Coven/static/logo.png")
+st.image("/East_Coast_Coven/static/logo.png")
